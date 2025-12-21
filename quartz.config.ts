8511d98fe133c5e5af7455f40374351d6557efc3
Tammam ALOUDAT <tammamo@tammams-macbook-pro.home>
2025-12-22 00:04:18 +0100

    Configure for humanitarian-bibliography.org custom domain

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Humanitarian Bibliography",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,8 +16,8 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    baseUrl: "humanitarian-bibliography.org",
+    ignorePatterns: ["private", "Templates", ".obsidian", ".DS_Store"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
